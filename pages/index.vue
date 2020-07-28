1846ac78ebc4398b71453c2c00bcfc0aee88c1b9
Aisylu Shakirova <aisilunya@gmail.com>
2020-07-28 16:53:29 +0200

    Add netlify identity

--- pages/index.vue
+++ pages/index.vue
@@ -14,12 +14,17 @@ import Banner from "~/components/Banner.vue";
 import Posts from "~/components/Posts.vue";
 
 export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
   components: {
     Header,
     Footer,
     Banner,
     Posts
-  }
+  },
 };
 </script>
 
